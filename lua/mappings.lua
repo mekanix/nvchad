53b73313ee9359758eb41544d402bbd04e9c9467
Goran Mekić <meka@tilda.center>
2026-01-31 19:23:18 +0100

    Add format on save and tab/spaces toggles

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,8 +3,39 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local autocmd = vim.api.nvim_create_autocmd
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+--
+
+map("n", "<leader>tf", function()
+  vim.g.disable_autoformat = not vim.g.disable_autoformat
+  print("Autoformat: " .. (vim.g.disable_autoformat and "OFF" or "ON"))
+end, { desc = "Toggle Autoformat on Save" })
+
+map("n", "<leader>tt", function()
+  if vim.opt.expandtab:get() then
+    vim.opt.tabstop = 8
+    vim.opt.shiftwidth = 8
+    vim.opt.expandtab = false
+    print("Tabs: 8 (Hard Tabs)")
+  else
+    vim.opt.tabstop = 2
+    vim.opt.shiftwidth = 2
+    vim.opt.expandtab = true
+    print("Tabs: 2 (Spaces)")
+  end
+end, { desc = "Toggle Tabs vs Spaces" })
+
+
+autocmd("FileType", {
+  pattern = { "c", "cpp", ".h" },
+  callback = function()
+    vim.opt_local.tabstop = 8
+    vim.opt_local.shiftwidth = 8
+    vim.opt_local.expandtab = false
+  end,
+})
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,8 +1,15 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
+    event = 'BufWritePre',
+    opts = {
+      format_on_save = function(bufnr)
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 500, lsp_fallback = true }
+      end,
+    },
   },
 
   -- These are some examples, uncomment them if you want to see them work!
@@ -13,6 +20,31 @@ return {
     end,
   },
 
+  {
+    "mrcjkb/rustaceanvim",
+    ft = { "rust" },
+    config = function()
+      -- Preuzmi standardne NvChad on_attach funkcije (za gd, gi, itd.)
+      local on_attach = require("nvchad.configs.lspconfig").on_attach
+
+      vim.g.rustaceanvim = {
+        server = {
+          on_attach = function(client, bufnr)
+            -- Aktiviraj standardne NvChad prečice
+            on_attach(client, bufnr)
+
+            -- Dodaj Rust specifične prečice
+            local map = vim.keymap.set
+            map("n", "<leader>ra", "<cmd>RustLsp codeAction<cr>", { buffer = bufnr, desc = "Rust Code Action" })
+            map("n", "<leader>rr", "<cmd>RustLsp runnables<cr>", { buffer = bufnr, desc = "Rust Run" })
+            map("n", "<leader>rd", "<cmd>RustLsp debuggables<cr>", { buffer = bufnr, desc = "Rust Debug" })
+            map("n", "<leader>rm", "<cmd>RustLsp expandMacro<cr>", { buffer = bufnr, desc = "Rust Expand Macro" })
+
+          end,
+        },
+      }
+    end,
+  },
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
